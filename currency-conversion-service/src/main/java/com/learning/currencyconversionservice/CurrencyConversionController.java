1136c1cdb44b0c68db2a0ca6b07ee6de9c3352de
Caglar Gonul <caglar.gonul.001@gmail.com>
2020-01-09 07:20:57 +0300

    Feign client has been implemented.

--- currency-conversion-service/src/main/java/com/learning/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/learning/currencyconversionservice/CurrencyConversionController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,14 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
 
 	@GetMapping ("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
-
-		// Feign - Problem 1
 		Map<String, String> uriVariables = new HashMap<>();
 		uriVariables.put("from", from);
 		uriVariables.put("to", to);
@@ -28,6 +31,13 @@ public class CurrencyConversionController {
 				uriVariables);
 
 		CurrencyConversionBean response = responseEntity.getBody();
+		assert response != null;
+		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
+	}
+
+	@GetMapping ("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+		CurrencyConversionBean response = currencyExchangeServiceProxy.retrieveExchangeValue(from, to);
 		assert response != null;
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
 	}
